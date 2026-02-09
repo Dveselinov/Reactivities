904b7bc99be531454a31923a7a2376e157570e93
Dveselinov <dveselinov@gmail.com>
2026-02-09 14:31:14 +0200

    End of section 4

---
+++ Application/Activities/Commands/DeleteAcivity.cs
@@ -0,0 +1,25 @@
+using System;
+using MediatR;
+using Peristence;
+
+namespace Application.Activities.Commands;
+
+public class DeleteAcivity
+{
+    public class Command : IRequest
+    {
+        public required string Id { get; set; }
+    }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    {
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities.FindAsync([request.Id], cancellationToken) ?? throw new Exception("Cannot find activity");
+
+            context.Remove(activity);
+            
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
